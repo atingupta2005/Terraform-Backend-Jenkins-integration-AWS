83b7a1b96ebfe250fc94d8c75b9da8b0e1e7b933
atingupta2005 <atingupta2005@gmail.com>
2021-09-30 18:59:15 +0530

    Update public.tf

--- public.tf
+++ public.tf
@@ -45,26 +45,8 @@ resource "aws_security_group" "web" {
     Name = "WebServerSG"
   }
 }
-
-/*
-resource "aws_instance" "web-1" {
-  ami                         = "${lookup(var.amis, var.aws_region)}"
-  availability_zone           = "us-east-1a"
-  instance_type               = "m1.small"
-  key_name                    = "${var.aws_key_name}"
-  vpc_security_group_ids      = ["${aws_security_group.web.id}"]
-  subnet_id                   = "${aws_subnet.us-east-1a-public.id}"
-  associate_public_ip_address = true
-  source_dest_check           = false
-
-  tags = {
-    Name = "Web Server 1"
-  }
-}
-*/
   
 
 resource "aws_eip" "web-1" {
-  #instance = "${aws_instance.web-1.id}"
   vpc      = true
 }
