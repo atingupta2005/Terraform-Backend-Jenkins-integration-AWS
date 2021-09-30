e49b3ba58bd9423aa08ce0079d102f5543c32539
atingupta2005 <atingupta2005@gmail.com>
2021-09-30 18:41:34 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "this" {
-  cidr_block           = var.cidr_block
+  #cidr_block           = var.cidr_block
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
