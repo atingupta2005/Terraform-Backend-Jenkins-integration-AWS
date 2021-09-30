cb15cd2c1ad3e578b7b613e7af4afa167fd4fa70
atingupta2005 <atingupta2005@gmail.com>
2021-09-30 18:45:48 +0530

    Update public.tf

--- public.tf
+++ public.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["${var.private_subnet_cidr}"]
   }
 
-  #vpc_id = "${aws_vpc.default.id}"
-  vpc_id = "${aws_vpc.id}"
+  vpc_id = "${aws_vpc.this.id}"
+  #vpc_id = "${aws_vpc.id}"
   
 
   tags = {
